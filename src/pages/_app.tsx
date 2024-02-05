6e7733eec28674b63400f52aa0196a93c0774ee0
Andy Parinas <andy.parinas@gmail.com>
2024-02-05 21:17:54 +1000

    Working Application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+import Layout from "@/components/Layout/layout";
+
+import "./styles.css";
+
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
+
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
